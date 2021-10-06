321b4bb8bf0df9a1457461ccfab586122452aeb6
Daniele Pastori <fullbl@mail.com>
2021-10-06 14:28:22 +0200

    remove Filaments

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ import VueRouter from 'vue-router'
 import Dashboard from './Control/Dashboard.vue'
 import Console from './Control/Console.vue'
 
-import Filaments from './Files/Filaments.vue'
 import Jobs from './Files/Jobs.vue'
 import Macros from './Files/Macros.vue'
 import System from './Files/System.vue'
@@ -182,7 +181,6 @@ Vue.use(Dashboard)
 Vue.use(Console)
 
 // Files
-Vue.use(Filaments)
 Vue.use(Jobs)
 Vue.use(Macros)
 Vue.use(System)
