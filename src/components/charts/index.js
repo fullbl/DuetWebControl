fd347e266f7800591a53b99a8bf1cfd2af58ff55
Daniele Pastori <fullbl@mail.com>
2021-10-06 14:29:29 +0200

    remove Temperature Chart

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import 'chartjs-adapter-date-fns'
 
 import LayerChart from './LayerChart.vue'
-import TemperatureChart from './TemperatureChart.vue'
 
 Vue.component('layer-chart', LayerChart)
-Vue.component('temperature-chart', TemperatureChart)
 
